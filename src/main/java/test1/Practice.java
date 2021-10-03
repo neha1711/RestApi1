02fe51d17d5c70314b59bd6f808d9897af4046ba
neha kumari <nehagupta1190@gmail.com>
2021-10-03 20:21:05 +0530

    change to practice class

--- src/main/java/test1/Practice.java
+++ src/main/java/test1/Practice.java
@@ -1,5 +1,16 @@
 package test1;
 
-public class Practice {
+import files.Payload;
+import io.restassured.path.json.JsonPath;
 
+public class Practice {
+	public static void main(String[] args) {
+	JsonPath js = new JsonPath(Payload.body2());
+	int coursecount=js.getInt("courses.size()");
+	System.out.println("coursecount");
+	int purchaseamt=js.getInt("dashboard.purchaseAmount");
+	System.out.println("purchaseamt");
+	String title1=js.getString("courses[0].title");
+	System.out.println("title1");
+}
 }
